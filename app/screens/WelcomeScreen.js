1a12276780a8e561056bf4ddf130b3d0e6a9cf58
Jesse Colligan <jesse.m.colligan@gmail.com>
2021-02-07 19:50:04 -0800

    change login button text color to black

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     loginButtonText: {
-        color: "white",
+        color: "black",
         fontWeight: "bold",
         fontSize: 26
     }
