88184cddbb0f4c58b6147c4ef731badcfa3024fc
Jesse Colligan <jesse.m.colligan@gmail.com>
2021-02-14 23:31:23 -0800

    basic entry of tabata settings on home page

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -5,11 +5,11 @@ import {MaterialCommunityIcons} from '@expo/vector-icons'
 import colors from '../config/colors'
 import defaultStyles from '../config/styles'
 
-export default function AppTextInput({icon, ...otherProps}) {
+export default function AppTextInput({icon, placeholder, ...otherProps}) {
     return (
         <View style={styles.container}>
-            {icon && <MaterialCommunityIcons name={icon}/>}
-            <TextInput style={defaultStyles.text} {...otherProps}/>
+            {icon && <MaterialCommunityIcons style={{marginRight: 10}} name={icon} color={colors.grayMedium} size={20}/>}
+            <TextInput style={defaultStyles.text} placeholder={placeholder} {...otherProps}/>
         </View>
     )
 }
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import AppPicker from '../components/AppPicker'
+import AppButton from '../components/AppButton'
+import AppTextInput from '../components/AppTextInput';
+
+// improvement: use a timer incrementer picker thing instead: https://www.npmjs.com/package/react-native-numeric-input
 
 const exercises = [
     { label: "Burpees", value: 1 },
@@ -11,10 +15,34 @@ const exercises = [
 
 export default function HomeScreen() {
     const [exercise, setExercise] = useState()
+    const [exerciseArr, setExerciseArr] = useState([])
+    const [numExercises, setNumExercises] = useState()
+    const [roundDuration, setRoundDuration] = useState()
+    const [breakDuration, setBreakDuration] = useState()
 
     return (
         <View>
+            <AppTextInput icon="timer-sand" placeholder={"Prep Time"}/>
+            <AppTextInput icon="timer" placeholder={"Round Duration"}/>
+            <AppTextInput icon="timer" placeholder={"Break Duration"}/>
+            <AppTextInput icon="repeat" placeholder={"Rounds (number of exercises)"} keyboardType='number-pad' onChangeText={(text) => setNumExercises(text)}/>
             <AppPicker selectedItem={exercise} onSelectItem={item => setExercise(item)} items={exercises} icon="weight-lifter" placeholder="Exercise 1"></AppPicker>
+            <AppPicker selectedItem={exercise} onSelectItem={item => setExercise(item)} items={exercises} icon="weight-lifter" placeholder="Exercise 1"></AppPicker>
+            <AppPicker selectedItem={exercise} onSelectItem={item => setExercise(item)} items={exercises} icon="weight-lifter" placeholder="Exercise 1"></AppPicker>
+            <AppPicker selectedItem={exercise} onSelectItem={item => setExercise(item)} items={exercises} icon="weight-lifter" placeholder="Exercise 1"></AppPicker>
+            <View style={styles.logoContainer}>
+                <AppButton title="Start Tabata" onPress={() => navigation.navigate('Home')} />
+            </View>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    logoContainer: {
+        top: 420,
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+})
