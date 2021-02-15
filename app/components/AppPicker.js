72001d3378f5cff88fea5081386f5c203ee153c5
Jesse Colligan <jesse.m.colligan@gmail.com>
2021-02-15 00:26:50 -0800

    basic Home page form done

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { StyleSheet, TouchableWithoutFeedback, View, Modal, Button, FlatList } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import {uid} from 'uid'
 
 import colors from '../config/colors'
 import AppText from './AppText'
@@ -15,7 +16,7 @@ export default function AppPicker({ icon, items, onSelectItem, placeholder, sele
             <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
                 <View style={styles.container}>
                     {icon && <MaterialCommunityIcons name={icon} size={20} color={colors.grayMedium} style={styles.icon} /> }
-                    <AppText style={styles.text}>{selectedItem ? selectedItem.label : placeholder}</AppText>
+                    <AppText style={styles.text}>{selectedItem ? selectedItem : placeholder}</AppText>
                     <MaterialCommunityIcons name="chevron-down" size={20} color={colors.grayMedium}/>
                 </View>
             </TouchableWithoutFeedback>
@@ -24,9 +25,9 @@ export default function AppPicker({ icon, items, onSelectItem, placeholder, sele
                     <Button title="Close" onPress={() => setModalVisible(false)}/>
                     <FlatList 
                         data={items}
-                        keyExtractor={item => item.value.toString()}
+                        keyExtractor={item => uid()}
                         renderItem={({item}) => <PickerItem
-                            label={item.label}
+                            label={item}
                             onPress={() => {
                                 setModalVisible(false)
                                 onSelectItem(item)
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { StyleSheet, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
+import { uid } from 'uid'
 
 import AppPicker from '../components/AppPicker'
 import AppButton from '../components/AppButton'
@@ -8,12 +9,17 @@ import AppTextInput from '../components/AppTextInput';
 
 // improvement: use a timer incrementer picker thing instead: https://www.npmjs.com/package/react-native-numeric-input
 
-const exercises = [
-    { label: "Burpees", value: 1 },
-    { label: "Squats", value: 2 },
-    { label: "Push Ups", value: 3 }
-]
 
+// to do: update this with a list of real exercises in alphabetical order (better if searchable also)
+const exercises = [
+    "Burpees",
+    "Squats",
+    "Push Ups",
+    "Jumping Jacks",
+    "Lunges",
+    "Plank",
+    "Pull Ups"
+]
 
 export default function HomeScreen() {
     const navigation = useNavigation()
@@ -23,16 +29,26 @@ export default function HomeScreen() {
     const [roundDuration, setRoundDuration] = useState()
     const [breakDuration, setBreakDuration] = useState()
 
+    const exerciseInputEles = []
+    for (let i = 0; i < numExercises; i++) {
+        exerciseInputEles.push(<AppPicker id={uid()} selectedItem={exerciseArr[i]} onSelectItem={item => updateExercise(item, i)} items={exercises} icon="weight-lifter" placeholder={`Exercise ${i + 1}`} />)
+    }
+
+    const updateExercise = (exercise, idx) => {
+        const newExerciseArr = [...exerciseArr];
+        let newExercise = { ...newExerciseArr[idx] };
+        newExercise = exercise
+        newExerciseArr[idx] = newExercise;
+        setExerciseArr(newExerciseArr);
+    }
+
     return (
         <View>
             <AppTextInput icon="timer-sand" placeholder={"Prep Time"} keyboardType='number-pad'/>
             <AppTextInput icon="timer" placeholder={"Round Duration"} keyboardType='number-pad' onChangeText={(text) => setRoundDuration(text)}/>
             <AppTextInput icon="timer" placeholder={"Break Duration"} keyboardType='number-pad' onChangeText={(text) => setBreakDuration(text)}/>
             <AppTextInput icon="repeat" placeholder={"Rounds (number of exercises)"} keyboardType='number-pad' onChangeText={(text) => setNumExercises(text)}/>
-            <AppPicker selectedItem={exercise} onSelectItem={item => setExercise(item)} items={exercises} icon="weight-lifter" placeholder="Exercise 1"></AppPicker>
-            <AppPicker selectedItem={exercise} onSelectItem={item => setExercise(item)} items={exercises} icon="weight-lifter" placeholder="Exercise 1"></AppPicker>
-            <AppPicker selectedItem={exercise} onSelectItem={item => setExercise(item)} items={exercises} icon="weight-lifter" placeholder="Exercise 1"></AppPicker>
-            <AppPicker selectedItem={exercise} onSelectItem={item => setExercise(item)} items={exercises} icon="weight-lifter" placeholder="Exercise 1"></AppPicker>
+            {exerciseInputEles}
             <View style={styles.startTabataContainer}>
                 <AppButton disabled title="Start Tabata" onPress={() => navigation.navigate('Tabata')} />
             </View>
