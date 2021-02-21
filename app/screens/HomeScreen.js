3899372218532f7a9c67c8aff95788a5c114c127
Jesse Colligan <jesse.m.colligan@gmail.com>
2021-02-20 22:24:36 -0800

    tabata screen logic working as expected

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
 import { uid } from 'uid'
 
 import AppPicker from '../components/AppPicker'
 import AppButton from '../components/AppButton'
 import AppTextInput from '../components/AppTextInput';
+import AppText from '../components/AppText';
+import Screen from '../components/Screen';
 
 // improvement: use a timer incrementer picker thing instead: https://www.npmjs.com/package/react-native-numeric-input
 
@@ -28,10 +30,11 @@ export default function HomeScreen() {
     const [numRounds, setNumRounds] = useState()
     const [roundDuration, setRoundDuration] = useState()
     const [breakDuration, setBreakDuration] = useState()
+    const [numSets, setNumSets] = useState()
 
     const exerciseInputEles = []
-    for (let i = 0; i < numRounds; i++) {
-        exerciseInputEles.push(<AppPicker id={uid()} selectedItem={exerciseArr[i]} onSelectItem={item => updateExercise(item, i)} items={exercises} icon="weight-lifter" placeholder={`Exercise ${i + 1}`} />)
+    for (let i = 0; i < numSets; i++) {
+        exerciseInputEles.push(<AppPicker key={uid()} selectedItem={exerciseArr[i]} onSelectItem={item => updateExercise(item, i)} items={exercises} icon="weight-lifter" placeholder={`Exercise ${i + 1}`} />)
     }
 
     const updateExercise = (exercise, idx) => {
@@ -43,19 +46,25 @@ export default function HomeScreen() {
     }
 
     return (
-        <View>
+        <Screen>
             <View style={styles.navBar}>
                 <Image style={styles.logo} source={require("../assets/tt-logo.png")} />
             </View>
             <AppTextInput icon="timer-sand" placeholder={"Prep Time"} keyboardType='number-pad' onChangeText={(text) => setPrepTime(text)}/>
             <AppTextInput icon="timer" placeholder={"Round Duration"} keyboardType='number-pad' onChangeText={(text) => setRoundDuration(text)}/>
             <AppTextInput icon="timer" placeholder={"Break Duration"} keyboardType='number-pad' onChangeText={(text) => setBreakDuration(text)}/>
-            <AppTextInput icon="repeat" placeholder={"Rounds (number of exercises)"} keyboardType='number-pad' onChangeText={(text) => setNumRounds(text)}/>
+            <AppTextInput icon="repeat" placeholder={"Number of Rounds"} keyboardType='number-pad' onChangeText={(text) => setNumRounds(text)}/>
+            <AppTextInput icon="format-list-numbered" placeholder={"Number of Sets"} keyboardType='number-pad' onChangeText={(text) => setNumSets(text)}/>
             {exerciseInputEles}
             <View style={styles.startTabataContainer}>
-                <AppButton disabled title="Start Tabata" onPress={() => navigation.navigate('Tabata', {prepTime: prepTime, numRounds: numRounds, roundDuration: roundDuration, breakDuration: breakDuration, exerciseArr: exerciseArr})} />
+                {!(prepTime && roundDuration && breakDuration && numRounds && numSets && exerciseArr.length > 0) && 
+                    <View>
+                        <Text style={styles.text}>Fill out all fields to start your Tabata Timer.</Text>
+                    </View>
+                }
+                {(prepTime && roundDuration && breakDuration && numRounds && numSets && exerciseArr.length > 0) && <AppButton title="Start Tabata" onPress={() => navigation.navigate('Tabata', {numSets: numSets, prepTime: prepTime, numRounds: numRounds, roundDuration: roundDuration, breakDuration: breakDuration, exerciseArr: exerciseArr})} />}
             </View>
-        </View>
+        </Screen>
     )
 }
 
@@ -76,4 +85,11 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: "center"
     },
+    text: {
+        width: "100%", // this width might cause problems
+        height: 20,
+        bottom: 160,
+        justifyContent: "center",
+        alignItems: "center",
+    }
 })
--- app/screens/TabataScreen.js
+++ app/screens/TabataScreen.js
@@ -1,28 +1,99 @@
 import React, {useState, useEffect} from 'react'
-import {Text} from 'react-native'
+import { Text, StyleSheet, View} from 'react-native'
 
 import Screen from '../components/Screen'
+import Constants from "expo-constants"
 
 export default function Tabata({route}) {
     const [timeRemaining, setTimeRemaining] = useState(route.params.prepTime)
+    const [phase, setPhase] = useState("prep")
+    const [numExercisesCompleted, setNumExercisesCompleted] = useState(0)
+    const [roundsCompleted, setRoundsCompleted] = useState(0)
 
     useEffect(() => {
-        if(timeRemaining > 0)
-            setTimeout(() => {
-                if (timeRemaining > 0) setTimeRemaining(timeRemaining - 1)
-                else console.log('next')// move to the next stage (handle in another function)
-            }, 1000)
+        if (numExercisesCompleted <= route.params.exerciseArr.length) {
+            // in a round
+            if(timeRemaining > 0)
+                setTimeout(() => {
+                    setTimeRemaining(timeRemaining - 1)
+                }, 100)
+            else if (numExercisesCompleted === 0) {
+                setNumExercisesCompleted(numExercisesCompleted + 1)
+                setPhase("round")
+                setTimeRemaining(route.params.roundDuration)
+            }
+            else if (phase === "break") {
+                setPhase("round")
+                setTimeRemaining(route.params.roundDuration)
+            } else if (phase === "round") {
+                setNumExercisesCompleted(numExercisesCompleted + 1)
+                setPhase("break")
+                setTimeRemaining(route.params.breakDuration)
+            }
+        } else if (roundsCompleted <= route.params.numRounds) {
+            // start a new round or end if all rounds complete
+            if (roundsCompleted === route.params.numRounds - 1) {
+                setPhase("end")
+            } else {
+                setRoundsCompleted(roundsCompleted + 1)
+                setNumExercisesCompleted(0)
+                setTimeRemaining(route.params.prepTime)
+                setPhase("prep")
+            }
+        } else {
+            setPhase("end")
+        }
     }, [timeRemaining])
 
 
     return (
-        <Screen>
-            <Text>Rounds: {route.params.numRounds}</Text>
-            <Text>Prep Time: {route.params.prepTime}</Text>
-            <Text>Round Duration: {route.params.roundDuration}</Text>
-            <Text>Break Duration: {route.params.breakDuration}</Text>
-            <Text>Exercises: {route.params.exerciseArr}</Text>
-            <Text>{timeRemaining}</Text>
-        </Screen>
+        <View>
+            <View style={styles.timerContainer}>
+
+                <Text>Exercises Completed: {numExercisesCompleted}</Text>
+                <Text>Rounds Completed: {roundsCompleted}</Text>
+                
+                {phase !== "end" && 
+                    <>
+                        <Text>Phase: {phase}</Text>
+                    <Text>Exercise: {numExercisesCompleted === 0 ? route.params.exerciseArr[0] : route.params.exerciseArr[numExercisesCompleted - 1]}</Text>
+                        <Text style={styles.timer}>{timeRemaining}</Text>
+                    </>
+                }
+
+                {phase === "end" && <Text style={styles.done}>Done!</Text>}
+
+            </View>
+            <View style={styles.configContainer}>
+                <Text>Rounds: {route.params.numRounds}</Text>
+                <Text>Prep Time: {route.params.prepTime}</Text>
+                <Text>Round Duration: {route.params.roundDuration}</Text>
+                <Text>Break Duration: {route.params.breakDuration}</Text>
+            </View>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: Constants.statusBarHeight
+    },
+    timer: {
+        fontSize: 200,
+        fontWeight: "bold"
+    },
+    done: {
+        fontSize: 80,
+        fontWeight: "bold"
+    },
+    timerContainer: {
+        backgroundColor: "green",
+        alignItems: "center"
+    },
+    configContainer: {
+        backgroundColor: "dodgerblue",
+        display: "flex"
+        // justifyContent: "flex-end",
+        // alignItems: "center"
+    }
+})
