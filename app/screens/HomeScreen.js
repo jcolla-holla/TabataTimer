0e64b426674982605b551744c13d642baaaf4a19
Jesse Colligan <jesse.m.colligan@gmail.com>
2021-02-20 23:15:29 -0800

    fixed bug of removing Prep Time value erroring out

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,12 +1,11 @@
 import React, {useState} from 'react';
-import { StyleSheet, View, Image, Text } from 'react-native';
+import { StyleSheet, View, Image, Text, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
 import { uid } from 'uid'
 
 import AppPicker from '../components/AppPicker'
 import AppButton from '../components/AppButton'
 import AppTextInput from '../components/AppTextInput';
-import AppText from '../components/AppText';
 import Screen from '../components/Screen';
 
 // improvement: use a timer incrementer picker thing instead: https://www.npmjs.com/package/react-native-numeric-input
@@ -47,23 +46,25 @@ export default function HomeScreen() {
 
     return (
         <Screen>
-            <View style={styles.navBar}>
-                <Image style={styles.logo} source={require("../assets/tt-logo.png")} />
-            </View>
-            <AppTextInput icon="timer-sand" placeholder={"Prep Time"} keyboardType='number-pad' onChangeText={(text) => setPrepTime(text)}/>
-            <AppTextInput icon="timer" placeholder={"Round Duration"} keyboardType='number-pad' onChangeText={(text) => setRoundDuration(text)}/>
-            <AppTextInput icon="timer" placeholder={"Break Duration"} keyboardType='number-pad' onChangeText={(text) => setBreakDuration(text)}/>
-            <AppTextInput icon="repeat" placeholder={"Number of Rounds"} keyboardType='number-pad' onChangeText={(text) => setNumRounds(text)}/>
-            <AppTextInput icon="format-list-numbered" placeholder={"Number of Sets"} keyboardType='number-pad' onChangeText={(text) => setNumSets(text)}/>
-            {exerciseInputEles}
-            <View style={styles.startTabataContainer}>
-                {!(prepTime && roundDuration && breakDuration && numRounds && numSets && exerciseArr.length > 0) && 
-                    <View>
-                        <Text style={styles.text}>Fill out all fields to start your Tabata Timer.</Text>
-                    </View>
-                }
-                {(prepTime && roundDuration && breakDuration && numRounds && numSets && exerciseArr.length > 0) && <AppButton title="Start Tabata" onPress={() => navigation.navigate('Tabata', {numSets: numSets, prepTime: prepTime, numRounds: numRounds, roundDuration: roundDuration, breakDuration: breakDuration, exerciseArr: exerciseArr})} />}
-            </View>
+            <ScrollView style={{ backgroundColor: 'pink'}}>
+                <View style={styles.navBar}>
+                    <Image style={styles.logo} source={require("../assets/tt-logo.png")} />
+                </View>
+                <AppTextInput icon="timer-sand" placeholder={"Prep Time"} keyboardType='number-pad' onChangeText={(text) => setPrepTime(text)}/>
+                <AppTextInput icon="timer" placeholder={"Round Duration"} keyboardType='number-pad' onChangeText={(text) => setRoundDuration(text)}/>
+                <AppTextInput icon="timer" placeholder={"Break Duration"} keyboardType='number-pad' onChangeText={(text) => setBreakDuration(text)}/>
+                <AppTextInput icon="repeat" placeholder={"Number of Rounds"} keyboardType='number-pad' onChangeText={(text) => setNumRounds(text)}/>
+                <AppTextInput icon="format-list-numbered" placeholder={"Number of Sets"} keyboardType='number-pad' onChangeText={(text) => setNumSets(text)}/>
+                {exerciseInputEles}
+                <View style={styles.startTabataContainer}>
+                    {!(prepTime !== undefined && roundDuration !== undefined && breakDuration !== undefined && numRounds !== undefined && numSets !== undefined && exerciseArr.length > 0) && 
+                        <View>
+                            <Text style={styles.text}>Fill out all fields to start your Tabata Timer.</Text>
+                        </View>
+                    }
+                    {(prepTime !== undefined && roundDuration !== undefined && breakDuration !== undefined && numRounds !== undefined && numSets !== undefined && exerciseArr.length > 0) && <AppButton title="Start Tabata" onPress={() => navigation.navigate('Tabata', {numSets: numSets, prepTime: prepTime, numRounds: numRounds, roundDuration: roundDuration, breakDuration: breakDuration, exerciseArr: exerciseArr})} />}
+                </View>
+            </ScrollView>
         </Screen>
     )
 }
