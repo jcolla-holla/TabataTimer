0585942e21e1398fdb6555acf7dd98898b0ff5f3
Jesse Colligan <jesse.m.colligan@gmail.com>
2021-02-21 09:42:22 -0800

    resolve logo misplacement on WelcomeScreen on some devices

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { ImageBackground, StyleSheet, View, Image, } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
+import Constants from "expo-constants"
+
 
 import AppButton from '../components/AppButton'
+import Screen from '../components/Screen'
 
 const image = { uri: "https://images.unsplash.com/photo-1580051745102-d33599f86c69?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=608&q=80"}
 
@@ -15,8 +18,8 @@ function WelcomeScreen() {
             style={styles.background}
             source={image}
         >
-            <View style={styles.logoContainer}>
-                <Image style={styles.logo} source={require("../assets/tt-logo.png")}/>
+            <View style={styles.navBar}>
+                <Image style={styles.logo} source={require("../assets/tt-logo.png")} />
             </View>
 
             <AppButton title="Start Workout" onPress={()=> navigation.navigate('Home')}/>
@@ -28,16 +31,18 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         justifyContent: "flex-end",
-        alignItems: "center"
-    },
-    logoContainer: {
-        top: 420,
-        position: "absolute", 
+        alignItems: "center",
+        paddingTop: Constants.statusBarHeight
     },
     logo: {
-        width: 300,
-        height: 200
-    }
+        width: 200,
+        height: 120,
+    },
+    navBar: {
+        flex: 1,
+        alignItems: 'center',
+        top: 20
+    },
 })
 
 export default WelcomeScreen;
